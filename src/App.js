a8421aab89faf16a0c9d9e30e28e409537e9d525
Rachana <yrachana03@gmail.com>
2022-09-27 16:21:35 +0530

    Added landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Register from './components/Register';
-import {Routes,Route,Redirect} from 'react-router-dom';
-import RegisterPlantAdmin from './components/RegisterPlantAdmin';
-import RegisterTechnician from './components/RegisterTechnician';
-function App() {
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
+
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <>
-      <div id="mainBody" className="container">
-        <Routes>
-        <Route path="/Register" element={<Register/>}>
-          <Route path="/Register/PlantAdmin" element={<RegisterPlantAdmin/>}/>
-          <Route path="/Register/Technician" element={<RegisterTechnician/>}/>
-        </Route>
-        </Routes>
-      </div>
-    </>
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
   );
 }
 
